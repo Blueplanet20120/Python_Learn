945b8d32591457ad9cf5fffc502b9a00a5a4d12e
perfume <yxll@qql.com>
2020-11-28 23:00:09 +0800

    225955-复习今日学习内容

--- Numbers-String Practise2-3.py
+++ Numbers-String Practise2-3.py
@@ -3,18 +3,23 @@
 #String
 a = 5
 b = 10
-
 c = 4.55
 d = 5.9033333
+# 注意：/和//的区别是 /保留小数点位数至少一位 而//取整数
+print(b//a)
+print(b/a)
+
 
 print(a+b)
 print(c+d)
-
+##变量的用法
 name = "intyuuuuyuuti990ooot"
 print(name)
 print(name[0]) #打印第一个字符
-print(len(name)) #计算字符串长度
+#计算字符串长度
+print(len(name))
 print(name[19])
+
 print(name[2:5]) #从2-5个字符串
 print(name*2) #打印2次
 print(name + "is a good boy")
