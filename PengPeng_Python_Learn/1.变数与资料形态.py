0068afc98d877c1c10b61b5335b1d5cd28a1e995
perfume <yxll@qql.com>
2020-12-02 21:46:08 +0800

    12022145-变数与资料形态

---
+++ PengPeng_Python_Learn/1.变数与资料形态.py
@@ -0,0 +1,35 @@
+# 资料 程式最基本的单位 有很多的分类 就叫资料形态
+
+# 数字
+334441.变数与资料形态
+3.4
+# 字串
+"测试中文"
+"Hello World"
+# 布林值
+True
+False
+# 有顺序、可变动的列表 List
+[3, 4, 5]
+["Hello", "World"]
+#有顺序，不可变动的列表 Tuple
+(3,4,5)
+("Hello","World")
+# 集合 Set
+{3, 4, 5}
+{"Hello", "World"}
+# 字典 Dictionary
+{"apple": "苹果", "data": "资料"}
+
+# 变数：用来储存资料的自定义名称
+#变数名称=资料
+x=3
+#print(资料）
+# print(3)
+# print(True)
+# print([1,2,3,4,5])
+print(x)
+x=True #取代旧的资料，可变化性
+print(x)
+x={3,4,5,6} #集合 Set
+print(x)
